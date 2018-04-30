2ba8dce8d1dc76bdf70d22b5c6b1fad856528d1f
kyle715 <kyle7150@gmail.com>
2018-04-30 03:06:45 -0400

    First commit on vvisual studio code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new BullDozer();
+            dozer.color = "yellow";
+            dozer.size = "tiny";
+            Console.WriteLine("The Bulldozer is " + dozer.color);
+        }
+    }
+    public class BullDozer
+    {
+        public string color { get; set;}
+        public string size { get; set;}
+    }
+}
