b442ec0544efc206943a0c9338837e71504dd323
kyle715 <kyle7150@gmail.com>
2018-05-01 03:14:37 -0400

    Finished OOP

--- Program.cs
+++ Program.cs
@@ -1,32 +1,75 @@
 ﻿using System;
 
-namespace Bulldozer
+namespace CarHomework
 {
     class Program
     {
 
         public static void Main(string[] args)
         {
-            var worker = new FactoryWorker();
-            worker.count(4);
-            worker.count(5);
+            var car = new car();
+            var SaloonCar = new SaloonCar();
+            var SaloonCar1 = new SaloonCar1(4.ToString());
+            var SaloonCar2 = new SaloonCar2(4.ToString(), "Nissan");
+            var SaloonCar3 = new SaloonCar3(4.ToString(), "Ford", "Purple");
+            SaloonCar.NumberOfSeats = "3";
+            Console.WriteLine("car 1 is " + car.color);
+            Console.WriteLine("The Saloon Car is " + SaloonCar.color + " And has this many seats " + SaloonCar.NumberOfSeats);
+            Console.WriteLine("SaloonCar1 has this many seats " + SaloonCar1.seats);
+            Console.WriteLine("SaloonCar2 has " + SaloonCar2.seats + " seats and is made by" + SaloonCar2.manufactuer);
+            Console.WriteLine("SaloonCar3 has "+ SaloonCar3.seats + " seats is made by " + SaloonCar3.manufactuer + " and its color is " + SaloonCar3.color);
         }
     }
-    public class FactoryWorker
+    public class car
     {
-        public FactoryWorker ()
+        public car() 
         {
-
+            this.color = "red";
+            this.manufactuer = "toyota";
         }
-        public void count (int number)
+        public string color {get; set;}
+        public string manufactuer {get; set;}
+    }
+    public class SaloonCar : car
+    {
+        public SaloonCar()
         {
-            if(Total == null)
-            Total = 0;
-
-            Total = Total + number;
-            Console.WriteLine(Total.ToString());
         }
+        public string NumberOfSeats {get; set;}
+    }
+    public class SaloonCar1 : car
+    {
+        
+        public string seats {get; set;}
+        public SaloonCar1(string _Seats)
+        {
+            this.seats = _Seats;
+        }
+    }
+    public class SaloonCar2
+    {
+        public string seats {get; set;}
 
-        public int Total {get; set;}
+        public string manufactuer {get; set;}
+
+        public SaloonCar2(string _seats, string _manufactuer)
+        {
+            this.seats = _seats;
+            this.manufactuer = _manufactuer;
+        }
+    }
+    public class SaloonCar3
+    {
+        public string seats {get; set;}
+        public string manufactuer {get; set;}
+        
+        public string color {get;set;}
+
+        public SaloonCar3(string _seats, string _manufactuer, string _color)
+        {
+            this.seats = _seats;
+            this.manufactuer = _manufactuer;
+            this.color = _color;
+        }
     }
 }
